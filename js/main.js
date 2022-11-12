59fa63f6ede448fa8cdf9e4b221cd212e4562fec
Emorinken <emorinkensam@gmail.com>
2022-11-12 09:21:44 -0800

    added animations

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const hamburger= document.querySelector("#hamburger")
 const close= document.querySelector("#close")
 const mobileMenu= document.querySelector(".mobile-menu")
-// console.log(mobileMenu)
+
 const showMobileNav=()=>{
 	mobileMenu.classList.add("show")
 	// console.log("show")
@@ -12,3 +12,34 @@ const hideMobileNav=()=>{
 }
 hamburger.addEventListener("click",showMobileNav)
 close.addEventListener("click",hideMobileNav)
+
+function reveal() {
+	var reveals = document.querySelectorAll(".reveal");
+	var show = document.querySelectorAll(".shows");
+	for (var i = 0; i < reveals.length; i++) {
+	  var windowHeight = window.innerHeight;
+	  var elementTop = reveals[i].getBoundingClientRect().top;
+	  var elementVisible = 150;
+	  if (elementTop < windowHeight - elementVisible) {
+		reveals[i].classList.add("active");
+	  } else {
+		reveals[i].classList.remove("active");
+	  }
+	}
+	for (var i = 0; i < show.length; i++) {
+	  var windowHeight = window.innerHeight;
+	  var elementTop = reveals[i].getBoundingClientRect().top;
+	  var elementVisible = 150;
+	  if (elementTop < windowHeight - elementVisible) {
+		reveals[i].classList.add("active");
+	  } else {
+		reveals[i].classList.remove("active");
+	  }
+	}
+  }
+
+  window.addEventListener("scroll", reveal);
+
+
+// To check the scroll position on page load
+reveal();
